8fc2ed6a333a0ba048cd50c230cdee9ae5c7d6fd
sebastian_kup <sebastian.kuppa@web.de>
2022-11-02 16:51:32 +0800

    added imports and argparser

--- segment.py
+++ segment.py
@@ -1,1 +1,14 @@
+import torchvision
+import numpy as np
 import torch
+import argparse
+import cv2
+
+import segmentation_utils
+from PIL import Image
+
+# init argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--i', '--input', help='path to input image/video')
+args = vars(parser.parse_args())
+
